eed3446e1a467a4604bb7cfecf8251d21e4f0981
Sai Akhil <akallutla@kodecorp.com>
2025-06-07 17:55:43 +0530

    images in view bids and vehicle verfication

--- src/app/vahaanbazar/vehicle-verification/vehicleVerification.js
+++ src/app/vahaanbazar/vehicle-verification/vehicleVerification.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
@@ -11,12 +11,35 @@ import useToastService from "../../../services/useToastService";
 import Cookies from "js-cookie";
 import useVehicleService from "../../../services/useVehicleService";
 import { Checkbox } from "primereact/checkbox";
+import { FaRegEye } from "react-icons/fa";
+import { Galleria } from "primereact/galleria";
 
 const VehicleVerification = () => {
   const { showLoadingToast, updateToSuccessToast, updateToErrorToast } = useToastService();
-  const { GetVehicleVerfication,ApproveVehicleVerification } = useVehicleService(); // Fetch bids using useBids service
+  const { GetVehicleVerfication, ApproveVehicleVerification } = useVehicleService(); // Fetch bids using useBids service
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [images, setImages] = useState([]); // State to hold images for the gallery
+  const galleria = useRef(null);
+
+  const responsiveOptions = [
+    {
+      breakpoint: "1500px",
+      numVisible: 5,
+    },
+    {
+      breakpoint: "1024px",
+      numVisible: 3,
+    },
+    {
+      breakpoint: "768px",
+      numVisible: 2,
+    },
+    {
+      breakpoint: "560px",
+      numVisible: 1,
+    },
+  ];
   const FetchVehicleVerification = async () => {
     setLoading(true); // Start loading spinner
     try {
@@ -37,8 +60,8 @@ const VehicleVerification = () => {
   const approveButtonTemplate = (rowData) => {
     const handleApprove = () => {
       const body = {
-      application_id: rowData.id,
-      registration_number: rowData.registration_number,
+        application_id: rowData.id,
+        registration_number: rowData.registration_number,
       };
       const toastId = showLoadingToast("Approving Vehicle...");
       ApproveVehicleVerification(body)
@@ -54,63 +77,92 @@ const VehicleVerification = () => {
     return <Button label="Approve" className="approval-button" style={{ backgroundColor: "#4CAF50", color: "white", border: "none" }} onClick={handleApprove} />;
   };
 
-    const isActiveInsurance = (rowData) => { 
-      return <Checkbox checked={rowData.insurance}  disable={"true"} className="p-checkbox-sm" />;
-    };
-    const isActiveGst = (rowData) => {
-  
-  
-      return <Checkbox checked={rowData.gst_applicable} disable={"true"} className="p-checkbox-sm" />;
-    };
-   
-    const isOrginalInvoice = (rowData) => {   
-      return <Checkbox checked={rowData.original_invoice}  disable={"true"} className="p-checkbox-sm" />;
-    };
+  const isActiveInsurance = (rowData) => {
+    return <Checkbox checked={rowData.insurance} disable={"true"} className="p-checkbox-sm" />;
+  };
+  const isActiveGst = (rowData) => {
+    return <Checkbox checked={rowData.gst_applicable} disable={"true"} className="p-checkbox-sm" />;
+  };
 
+  const isOrginalInvoice = (rowData) => {
+    return <Checkbox checked={rowData.original_invoice} disable={"true"} className="p-checkbox-sm" />;
+  };
 
+  const itemTemplate = (item) => <img src={item.itemImageSrc} alt={item.alt} style={{ width: "100%", display: "block" }} />;
+
+  const thumbnailTemplate = (item) => <img src={item.itemImageSrc} alt={item.alt} style={{ display: "block" }} />;
+  const openGallery = (rowData) => {
+    if (galleria.current) {
+      galleria.current.show();  
+      setImages(rowData.fitness_images.map((url) => ({ itemImageSrc: url.image, alt: "Vehicle Image" })));
+    }
+  };
+  const imageBody = (rowData) => (
+    <div className="d-flex justify-content-center link_pointer" onClick={() => openGallery(rowData)}>
+      <FaRegEye color={"blue"} size={17} />
+    </div>
+  );
 
   return (
-    <div className="viewDetailsSection">
-      <div className="flex justify-between items-center ">
-        <h2 className="text-xl font-bold">Vehicle Verification</h2>
-      </div>
-      <Divider />
+    <>
+      <Galleria
+        ref={galleria}
+        value={images}
+        responsiveOptions={responsiveOptions}
+        numVisible={9}
+        style={{ maxWidth: "50%" }}
+        circular
+        fullScreen
+        showItemNavigators
+        item={itemTemplate}
+        thumbnail={thumbnailTemplate}
+        onHide={() => {
+          galleria.current.hide();
+          setImages([]);
+        }}
+      />
+      <div className="viewDetailsSection">
+        <div className="flex justify-between items-center ">
+          <h2 className="text-xl font-bold">Vehicle Verification</h2>
+        </div>
+        <Divider />
 
-      <div className="h-full">
-        <DataTable
-          value={data}
-          columnResizeMode="expand"
-          scrollable
-          scrollHeight="flex" // Vertical scroll
-          className="p-datatable-gridlines h-100"
-          loading={loading} // Show loading spinner
-          lazy
-        >
-          <Column field="id" header="ID" bodyStyle={{ minWidth: "60px", maxWidth: "80px" }} />
-          <Column field="category" header="Category" bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} />
-          <Column field="price" header="Price" bodyStyle={{ minWidth: "120px", maxWidth: "140px" }} align={"right"} />
-          <Column field="registration_number" header="Registration Number" bodyStyle={{ minWidth: "150px", maxWidth: "180px" }} align={"right"}/>
-          <Column field="state" header="State" bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} />
-          <Column field="brand" header="Brand" bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} />
-          <Column field="no_of_tyres" header="No. of Tyres" bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} align={"right"}/>
-          <Column field="chassis_number" header="Chassis Number" bodyStyle={{ minWidth: "150px", maxWidth: "180px" }} />
-          <Column field="location" header="Location" bodyStyle={{ minWidth: "120px", maxWidth: "140px" }} />
-          <Column field="original_invoice" header="Original Invoice" body={isOrginalInvoice} bodyStyle={{ minWidth: "120px", maxWidth: "140px" }} align={"center"} />
-          <Column field="asset_description" header="Asset Description" bodyStyle={{ minWidth: "150px", maxWidth: "200px" }} />
-          <Column field="year_of_manufacturing" header="Manufacturing(Yrs)" bodyStyle={{ minWidth: "120px", maxWidth: "170px" }} align={"center"} />
-          <Column field="odometer" header="Odometer(KM)" bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} align={"right"}   />
-          <Column field="insurance" header="Insurance" body={isActiveInsurance} bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} align={"center"}/>
-          <Column field="gst_applicable" header="GST Applicable" body={isActiveGst} bodyStyle={{ minWidth: "120px", maxWidth: "140px" }} align={"center"} />
-          <Column field="" header="Images" bodyStyle={{ minWidth: "50px" }} />
-          <Column
-            header="Approval"
-            body={(rowData) => (!rowData.verified ? approveButtonTemplate(rowData) : <span className="text-green-700">Approved</span>)}
-            bodyStyle={{ minWidth: "150px", textAlign: "center" }}
-            align={"center"}
-          />
-        </DataTable>
+        <div className="h-full">
+          <DataTable
+            value={data}
+            columnResizeMode="expand"
+            scrollable
+            scrollHeight="flex" // Vertical scroll
+            className="p-datatable-gridlines h-100"
+            loading={loading} // Show loading spinner
+            lazy
+          >
+            <Column field="id" header="ID" bodyStyle={{ minWidth: "60px", maxWidth: "80px" }} />
+            <Column field="category" header="Category" bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} />
+            <Column field="price" header="Price" bodyStyle={{ minWidth: "120px", maxWidth: "140px" }} align={"right"} />
+            <Column field="registration_number" header="Registration Number" bodyStyle={{ minWidth: "150px", maxWidth: "180px" }} align={"right"} />
+            <Column field="state" header="State" bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} />
+            <Column field="brand" header="Brand" bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} />
+            <Column field="no_of_tyres" header="No. of Tyres" bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} align={"right"} />
+            <Column field="chassis_number" header="Chassis Number" bodyStyle={{ minWidth: "150px", maxWidth: "180px" }} />
+            <Column field="location" header="Location" bodyStyle={{ minWidth: "120px", maxWidth: "140px" }} />
+            <Column field="original_invoice" header="Original Invoice" body={isOrginalInvoice} bodyStyle={{ minWidth: "120px", maxWidth: "140px" }} align={"center"} />
+            <Column field="asset_description" header="Asset Description" bodyStyle={{ minWidth: "150px", maxWidth: "200px" }} />
+            <Column field="year_of_manufacturing" header="Manufacturing(Yrs)" bodyStyle={{ minWidth: "120px", maxWidth: "170px" }} align={"center"} />
+            <Column field="odometer" header="Odometer(KM)" bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} align={"right"} />
+            <Column field="insurance" header="Insurance" body={isActiveInsurance} bodyStyle={{ minWidth: "100px", maxWidth: "120px" }} align={"center"} />
+            <Column field="gst_applicable" header="GST Applicable" body={isActiveGst} bodyStyle={{ minWidth: "120px", maxWidth: "140px" }} align={"center"} />
+            <Column field="" header="Images" bodyStyle={{ minWidth: "50px" }} body={imageBody} />
+            <Column
+              header="Approval"
+              body={(rowData) => (!rowData.verified ? approveButtonTemplate(rowData) : <span className="text-green-700 font-bold">Approved</span>)}
+              bodyStyle={{ minWidth: "150px", textAlign: "center" }}
+              align={"center"}
+            />
+          </DataTable>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/app/vahaanbazar/view-bids/viewBids.js
+++ src/app/vahaanbazar/view-bids/viewBids.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { ColumnGroup } from "primereact/columngroup";
@@ -12,17 +12,41 @@ import { Divider } from "primereact/divider";
 import useBids from "../../../services/useBids";
 import useToastService from "../../../services/useToastService";
 import Cookies from "js-cookie";
+import { Galleria } from "primereact/galleria";
+import { FaRegEye } from "react-icons/fa";
 
 const ViewBids = () => {
   const { showLoadingToast, updateToSuccessToast, updateToErrorToast } = useToastService();
-  const { GetBids,ApproveBid } = useBids(); // Fetch bids using useBids service
+  const { GetBids, ApproveBid } = useBids(); // Fetch bids using useBids service
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+ const[images, setImages] = useState([]); // State to hold images for the gallery
+  const galleria = useRef(null);
+
+  const responsiveOptions = [
+    {
+      breakpoint: "1500px",
+      numVisible: 5,
+    },
+    {
+      breakpoint: "1024px",
+      numVisible: 3,
+    },
+    {
+      breakpoint: "768px",
+      numVisible: 2,
+    },
+    {
+      breakpoint: "560px",
+      numVisible: 1,
+    },
+  ];
+
   const FetchBids = async () => {
     setLoading(true); // Start loading spinner
     try {
       const response = await GetBids();
-      if(response || response.data)  setData(response.data); // Set the fetched data
+      if (response || response.data) setData(response.data); // Set the fetched data
       setLoading(false); // Stop loading spinner
     } catch (error) {
       console.error("Error fetching bids:", error.message);
@@ -34,37 +58,27 @@ const ViewBids = () => {
     FetchBids();
   }, [Cookies.get("AccessToken")]);
 
-   // Approve Button Renderer
-   const approveButtonTemplate = (rowData) => {
+  // Approve Button Renderer
+  const approveButtonTemplate = (rowData) => {
     const handleApprove = () => {
       const body = {
-        registration_no: rowData.vehicle_details.registration_no, 
+        registration_no: rowData.vehicle_details.registration_no,
         auction_id: rowData.auction_id,
         vehicle_id: rowData.vehicle_id,
-        bid_id:rowData.bid_id,
-    }
-    const toastId = showLoadingToast("Approving Bid...");
+        bid_id: rowData.bid_id,
+      };
+      const toastId = showLoadingToast("Approving Bid...");
       ApproveBid(body)
         .then((response) => {
-          if(response)
-          updateToSuccessToast(toastId , "Bid approved successfully");
-          FetchBids()
+          if (response) updateToSuccessToast(toastId, "Bid approved successfully");
+          FetchBids();
         })
         .catch(() => {
           updateToErrorToast(toastId, "Error approving bid");
         });
     };
 
-    return (
-      <Button
-        label="Approve"
-        className="approval-button"
-        
-        style={{ backgroundColor: "#4CAF50", color: "white" ,border :"none"}} 
-        
-        onClick={handleApprove}
-      />
-    );
+    return <Button label="Approve" className="approval-button" style={{ backgroundColor: "#4CAF50", color: "white", border: "none" }} onClick={handleApprove} />;
   };
 
   const statusBodyTemplate = (rowData) => {
@@ -82,7 +96,7 @@ const ViewBids = () => {
       </span>
     );
   };
- 
+
   // Define the header group
   const headerGroup = (
     <ColumnGroup>
@@ -125,65 +139,115 @@ const ViewBids = () => {
     </ColumnGroup>
   );
 
-  return (
-    <div className="viewDetailsSection">
-      <div className="flex justify-between items-center ">
-        <h2 className="text-xl font-bold">View Bids</h2>
-      </div>
-      <Divider />
+ const itemTemplate = (item) => (
+  <img src={item.itemImageSrc} alt={item.alt} style={{ width: "100%", display: "block" }} />
+);
 
-      <div className="h-full">
-        <DataTable
-          value={data}
-          columnResizeMode="expand"
-          scrollable
-          scrollHeight="flex" // Vertical scroll
-          headerColumnGroup={headerGroup}
-          className="p-datatable-gridlines h-100"
-          loading={loading} // Show loading spinner
-          lazy
-        >
-          {/* Bid Details Columns */}
-          <Column field="auction_id" header="Auction ID" bodyStyle={{ minWidth: "80px" }} />
-          <Column field="bid_id" header="Bid ID" bodyStyle={{ minWidth: "80px" }} />
-          <Column field="vehicle_id" header="Vehicle ID" bodyStyle={{ minWidth: "90px" }} />
-          <Column field="bid_amount" header="Bid Amount" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="status" header="Status" bodyStyle={{ minWidth: "120px" }} body={statusBodyTemplate} />
+const thumbnailTemplate = (item) => (
+  <img src={item.itemImageSrc} alt={item.alt} style={{ display: "block" }} />
+);
 
-          {/* Vehicle Details Columns */}
-          <Column field="vehicle_details.registration_no" header="Registration No" bodyStyle={{ minWidth: "200px" }} />
-          <Column field="vehicle_details.make" header="Make" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="vehicle_details.model" header="Model" bodyStyle={{ minWidth: "150px" }} />
-          <Column field="vehicle_details.year" header="Year" bodyStyle={{ minWidth: "100px" }} />
-          <Column field="vehicle_details.fuel_type" header="Fuel Type" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="vehicle_details.state" header="State" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="vehicle_details.city" header="City" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="vehicle_details.yard_name" header="Yard Name" bodyStyle={{ minWidth: "150px" }} />
-          <Column field="vehicle_details.rc_availability" header="RC Availability" bodyStyle={{ minWidth: "150px" }} />
-          <Column field="vehicle_details.chassis_no" header="Chassis No" bodyStyle={{ minWidth: "150px" }} />
-          <Column field="vehicle_details.engine_no" header="Engine No" bodyStyle={{ minWidth: "150px" }} />
-          <Column field="vehicle_details.owner" header="Owner" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="vehicle_details.reg_date" header="Reg Date" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="vehicle_details.market_value" header="Market Value" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="vehicle_details.min_price" header="Min Price" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="vehicle_details.max_bids" header="Max Bids" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="vehicle_details.current_highest_bid" header="Current Highest Bid" bodyStyle={{ minWidth: "150px" }} />
-          <Column field="vehicle_details.reserve_price" header="Reserve Price" bodyStyle={{ minWidth: "150px" }} />
-          <Column field="vehicle_details.seller_reference" header="Seller Reference" bodyStyle={{ minWidth: "150px" }} />
-          <Column field="vehicle_details.repo_date" header="Repo Date" bodyStyle={{ minWidth: "120px" }} />
-          <Column field="vehicle_details.parking_charges" header="Parking Charges" bodyStyle={{ minWidth: "150px" }} />
-          <Column field="vehicle_details.transaction_fees" header="Transaction Fees" bodyStyle={{ minWidth: "150px" }} />
-          <Column field="vehicle_details.contact_person" header="Contact Person" bodyStyle={{ minWidth: "150px" }} />
-          <Column field=""  header="Images" bodyStyle={{ minWidth: "50px" }} />
-          <Column
-            header="Actions"
-            body={(rowData)=>rowData.status !== "approved" ? approveButtonTemplate(rowData) : null}
-            bodyStyle={{ minWidth: "150px", textAlign: "center" }}
-            align={"center"}
-          />
-        </DataTable>
-      </div>
+ const openGallery = (rowData) => {
+  if (galleria.current) {
+    galleria.current.show();
+    // Parse the JSON string to get the array of URLs
+    let urls = [];
+    try {
+      const imgObj = rowData.vehicle_details.images?.[0];
+      if (imgObj && typeof imgObj.image_url === "string") {
+        urls = JSON.parse(imgObj.image_url);
+      }
+    } catch (e) {
+      urls = [];
+    }
+    // Set images as array of objects for Galleria
+    setImages(urls.map(url => ({ itemImageSrc: url, alt: "Vehicle Image" })));
+  }
+};
+
+  const imageBody = (rowData) => (
+    <div className="d-flex justify-content-center link_pointer" onClick={()=>openGallery(rowData)}>
+      <FaRegEye color={"blue"} size={17} />
     </div>
+  );
+
+  return (
+    <>
+      <Galleria
+        ref={galleria}
+        value={images}
+        responsiveOptions={responsiveOptions}
+        numVisible={9}
+        style={{ maxWidth: "50%" }}
+        circular
+        fullScreen
+        showItemNavigators
+        item={itemTemplate}
+        thumbnail={thumbnailTemplate}
+        onHide={()=>{
+          galleria.current.hide()
+          setImages([])
+        }}
+      />
+      <div className="viewDetailsSection">
+        <div className="flex justify-between items-center ">
+          <h2 className="text-xl font-bold">View Bids</h2>
+        </div>
+        <Divider />
+
+        <div className="h-full">
+          <DataTable
+            value={data}
+            columnResizeMode="expand"
+            scrollable
+            scrollHeight="flex" // Vertical scroll
+            headerColumnGroup={headerGroup}
+            className="p-datatable-gridlines h-100"
+            loading={loading} // Show loading spinner
+            lazy
+          >
+            {/* Bid Details Columns */}
+            <Column field="auction_id" header="Auction ID" bodyStyle={{ minWidth: "80px" }} />
+            <Column field="bid_id" header="Bid ID" bodyStyle={{ minWidth: "80px" }} />
+            <Column field="vehicle_id" header="Vehicle ID" bodyStyle={{ minWidth: "90px" }} />
+            <Column field="bid_amount" header="Bid Amount" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="status" header="Status" bodyStyle={{ minWidth: "120px" }} body={statusBodyTemplate} />
+
+            {/* Vehicle Details Columns */}
+            <Column field="vehicle_details.registration_no" header="Registration No" bodyStyle={{ minWidth: "200px" }} />
+            <Column field="vehicle_details.make" header="Make" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="vehicle_details.model" header="Model" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="vehicle_details.year" header="Year" bodyStyle={{ minWidth: "100px" }} />
+            <Column field="vehicle_details.fuel_type" header="Fuel Type" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="vehicle_details.state" header="State" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="vehicle_details.city" header="City" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="vehicle_details.yard_name" header="Yard Name" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="vehicle_details.rc_availability" header="RC Availability" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="vehicle_details.chassis_no" header="Chassis No" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="vehicle_details.engine_no" header="Engine No" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="vehicle_details.owner" header="Owner" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="vehicle_details.reg_date" header="Reg Date" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="vehicle_details.market_value" header="Market Value" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="vehicle_details.min_price" header="Min Price" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="vehicle_details.max_bids" header="Max Bids" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="vehicle_details.current_highest_bid" header="Current Highest Bid" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="vehicle_details.reserve_price" header="Reserve Price" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="vehicle_details.seller_reference" header="Seller Reference" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="vehicle_details.repo_date" header="Repo Date" bodyStyle={{ minWidth: "120px" }} />
+            <Column field="vehicle_details.parking_charges" header="Parking Charges" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="vehicle_details.transaction_fees" header="Transaction Fees" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="vehicle_details.contact_person" header="Contact Person" bodyStyle={{ minWidth: "150px" }} />
+            <Column field="" header="Images" bodyStyle={{ minWidth: "50px" }} body={imageBody} />
+            <Column
+              header="Actions"
+              body={(rowData) => (rowData.status !== "approved" ? approveButtonTemplate(rowData) : null)}
+              bodyStyle={{ minWidth: "150px", textAlign: "center" }}
+              align={"center"}
+            />
+          </DataTable>
+        </div>
+      </div>
+    </>
   );
 };
 
